b73deca625fd549ba613bd57f42f8f0dec7ce036
Pinar Olguc <pinarolguc@gmail.com>
2023-08-31 16:25:19 +0300

    Initial project

---
+++ Stories/StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesApp.swift
+//  Stories
+//
+//  Created by Pinar Olguc on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
